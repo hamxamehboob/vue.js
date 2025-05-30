07ac605e4a4dbaeaf732b0316f2f1fdfe194bd9d
syedibad841 <syedibad841@gmail.com>
2025-05-30 10:47:04 +0500

    TextAreaContext.vue

---
+++ TextAreaContext.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative mt-5 w-full bg-white border border-solid border-zinc-300 min-h-[97px]">
+    <div class="absolute right-2.5 z-0 w-1 rounded bg-zinc-300 h-[26px] min-h-[26px] top-[13px]"></div>
+    <div class="absolute left-1.5 z-0 text-xs top-[18px] h-[84px] w-[361px]">
+      <p>
+        제1조(목적)<br />
+        이 약관은 주식회사 알림봇 (전자상거래 사업자)가 운영하는 스마트업(이하 "사이트"라 한다)에서 제공하는 인터넷 관련 서비스(이하 "서비스"라 한다)를 이용함에 있어 온라인 사이트와 이용자의 권리.의무 및 책임사항을 규정함을 목적으로 합니다.<br />
+        ※「PC통신, 무선 등을 이용하는 전자상거래에 대해서도 그 성질에 반하지 않는
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextContentArea'
+}
+</script>
